dd057558e93a8903f2647a856db92851b3c7302d
hengkysuryaa <hengkysurya88@gmail.com>
2020-10-21 15:05:58 +0700

    implementasi backend detail coklat

---
+++ ChocoAddStockDetail.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Detail page </title>
+        <link rel="stylesheet" href="choco-detail.css">
+        <script src="choco-detail.js"> </script>
+    </head>
+    <body>
+        <div class="flex-container">
+            <h3> Add Stock </h3>
+        </div>
+        
+        <div class="flex-container">
+            <table border="0" style="height:500px; width:100%;">
+                <?php 
+
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "praktikum";
+                    $dbname = "willywangky";
+
+                    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+                    if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                    }
+
+                    $sql = "SELECT * FROM coklat WHERE id=1;";
+                    $result = mysqli_query($conn, $sql);
+
+                    $row = mysqli_fetch_assoc($result);
+
+                    $url = urlencode("ferrero.jpg"); //TODO: assign dg hasil query row["urlpath"]
+
+                    echo "<tr>";
+                    echo "<td rowspan='7' class='picture-container' style='background-image: url($url);'> </td>";
+                    echo "<td>".$row["nama"]."</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td> Amount sold: ".$row["amount_sold"]."</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td> Price: ".$row["price"]."</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td> Amount remaining: ".$row["stock"]."</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td> Description </td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>".$row["deskripsi"]."</td>";
+                    echo "</tr>";
+                ?>  
+                <tr>
+                    <td style="height: 150px;"> 
+                        <p> Amount to add: </p>
+                        <form action="addstockprocess.php" method="POST"> 
+                            <input class="plus-minus-button" type="button" onclick="minusButton()" value="-">
+                            <input type="number" id="quantity" name="quantity" value="1" style="text-align: center;" readonly required>
+                            <input class="plus-minus-button" type="button" onclick="plusButton()" value="+">  <br> <br>             
+                            <button class="btn-add" type="submit"> <b> Add </b> </button>
+                        </form>
+                        <button class="btn-cancel" type="submit" onclick="location.href='ChocoDetailSuperuser.php'"> <b> Cancel </b> </button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
---
+++ ChocoDetailUser.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Detail page </title>
+        <link rel="stylesheet" href="choco-detail.css">
+    </head>
+    <body>
+        <?php 
+            $servername = "localhost";
+            $username = "root";
+            $password = "praktikum";
+            $dbname = "willywangky";
+
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            $sql = "SELECT * FROM coklat WHERE id=1;";
+            $result = mysqli_query($conn, $sql);
+
+            $row = mysqli_fetch_assoc($result);
+
+            echo "<div class='flex-container'>";
+            echo "<h3> Ferrero </h3>";
+            echo "</div>";
+                
+            echo "<div class='flex-container'>";
+            echo "<table border='0' style='height:400px; width:100%;'>";
+
+            $url = urlencode("ferrero.jpg"); //TODO: assign dg hasil query row["urlpath"]
+
+            echo "<tr>";
+            echo "<td rowspan='5' class='picture-container' style='background-image: url($url);'> </td>";
+            echo "<td> Amount sold: ".$row["amount_sold"]."</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<td> Price: ".$row["price"]."</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<td> Amount: ".$row["stock"]."</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<td> Description </td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<td>".$row["deskripsi"]."</td>";
+            echo "</tr>";
+            echo "</table>";
+            echo "</div> <br>";
+        ?>
+        <button class="btn-add" onclick="location.href = 'ChocoBuyDetail.php'"> <b> Buy Now </b> </button>
+    </body>
+</html>
