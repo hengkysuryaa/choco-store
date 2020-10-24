a2dd017d2354ce283d7ca8dff412417bcc9345a1
donbasta <faddilafarras@gmail.com>
2020-10-24 17:51:55 +0700

    integrate search with dashboard

--- php/SearchPage.php
+++ php/SearchPage.php
@@ -1,41 +1,72 @@
-<?php 
-
-    include 'configDB.php';
-
-    $content_per_page = 3;
-    $page = isset($_GET["page"]) ? (int)$_GET["page"] : 1;
-    $start = ($page>1) ? ($page * $content_per_page) - $content_per_page : 0;
-
-    $sql = "SELECT * FROM coklat";
-    $result = mysqli_query($conn, $sql);
-    $total_row = mysqli_num_rows($result);
-
-    $total_page = ceil($total_row/$content_per_page);
-
-    $sql2 = "SELECT * FROM coklat LIMIT $start, $content_per_page";
-    $result2 = mysqli_query($conn, $sql2);
-    
-
-    while ($row = mysqli_fetch_assoc($result2)) {
-
-        echo "<center>";
-        echo "<table border='1'>";
-        echo "<tr>";
-        echo "<td>".$row["nama"]."</td>";
-        echo "<td> Price: ".$row["price"]."</td>";
-        echo "</tr>";
-        echo "</table>";
-        echo "<br>";
-        echo "</center>";
-
-    }
+<?php
+  $getusername = include('getusername.php');
+  $username = $getusername($_COOKIE["currentUsername"]);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../styles/dashboard.css">
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li class="logout-link"><a href="logout.php">Logout</a></li>
+    </ul>
+
+    <br>
+    <br>
+    <br>
+    
+      <?php 
+
+        include 'connectDB.php';
+
+        $content_per_page = 4;
+        $page = isset($_GET["page"]) ? (int)$_GET["page"] : 1;
+        $start = ($page>1) ? ($page * $content_per_page) - $content_per_page : 0;
+
+        $search_query = $_GET["search"];
+
+        echo "TESUTO" . $search_query;
+
+        $sql = "SELECT * FROM coklat WHERE choco_name LIKE '%$search_query%'";
+        $result = mysqli_query($conn, $sql);
+        $total_row = mysqli_num_rows($result);
+
+        $total_page = ceil($total_row / $content_per_page);
+
+        $sql2 = "SELECT * FROM coklat WHERE choco_name LIKE '%$search_query%' LIMIT $start, $content_per_page";
+        $result2 = mysqli_query($conn, $sql2);
+
+        while ($row = mysqli_fetch_assoc($result2)) {
+
+            echo "<center>";
+            echo "<table border='1'>";
+            echo "<tr>";
+            echo "<td>".$row["choco_name"]."</td>";
+            echo "<td> Price: ".$row["price"]."</td>";
+            echo "</tr>";
+            echo "</table>";
+            echo "<br>";
+            echo "</center>";
+
+        }
+    ?>
 <br>
 <center> <div>
     <?php 
     
         for($i=1; $i<=$total_page; $i++) {
-            echo "<a href='?page=$i'>".$i."</a>";
+            echo "<a href='?page=$i'>    ".$i."   </a>";
         }
     ?>
 </div> </center>
+
+
+</body>
+</html>
